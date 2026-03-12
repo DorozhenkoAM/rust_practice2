52472c38f994b708bf62a79f9693f696d68b9ad5
DorozhenkoAM <andrii.dorozhenko@e-u.edu.ua>
2026-03-12 20:34:36 +0200

    task04

--- src/hackerrank/task04.rs
+++ src/hackerrank/task04.rs
@@ -1,1 +1,47 @@
-// https://www.hackerrank.com/challenges/grading/problem
+use std::env;
+use std::fs::File;
+use std::io::{self, BufRead, Write};
+
+
+fn gradingStudents(grades: &[i32]) -> Vec<i32> {
+    grades.iter().map(|&g| {
+        if g < 38 {
+            g
+        } else {
+            let next_multiple = ((g + 4) / 5) * 5; 
+            if next_multiple - g < 3 {
+                next_multiple
+            } else {
+                g
+            }
+        }
+    }).collect()
+}
+
+fn main() {
+    let stdin = io::stdin();
+    let mut stdin_iterator = stdin.lock().lines();
+
+    let mut fptr = File::create(env::var("OUTPUT_PATH").unwrap()).unwrap();
+
+    let grades_count = stdin_iterator.next().unwrap().unwrap().trim().parse::<i32>().unwrap();
+
+    let mut grades: Vec<i32> = Vec::with_capacity(grades_count as usize);
+
+    for _ in 0..grades_count {
+        let grades_item = stdin_iterator.next().unwrap().unwrap().trim().parse::<i32>().unwrap();
+        grades.push(grades_item);
+    }
+
+    let result = gradingStudents(&grades);
+
+    for i in 0..result.len() {
+        write!(&mut fptr, "{}", result[i]).ok();
+
+        if i != result.len() - 1 {
+            writeln!(&mut fptr).ok();
+        }
+    }
+
+    writeln!(&mut fptr).ok();
+}
