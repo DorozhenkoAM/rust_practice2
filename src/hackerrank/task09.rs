24fe900902c345df3a31a088d8e10d51eff69e68
DorozhenkoAM <andrii.dorozhenko@e-u.edu.ua>
2026-03-12 21:40:09 +0200

    task09

--- src/hackerrank/task09.rs
+++ src/hackerrank/task09.rs
@@ -1,1 +1,43 @@
-// https://www.hackerrank.com/challenges/migratory-birds/problem
+use std::env;
+use std::fs::File;
+use std::io::{self, BufRead, Write};
+
+
+fn migratoryBirds(arr: &[i32]) -> i32 {
+    let mut counts = [0; 6]; 
+    
+    for &bird in arr {
+        counts[bird as usize] += 1;
+    }
+    
+    let mut max_count = 0;
+    let mut best_type = 0;
+    
+    for i in 1..=5 {
+        if counts[i] > max_count {
+            max_count = counts[i];
+            best_type = i as i32;
+        }
+    }
+    
+    best_type
+}
+
+fn main() {
+    let stdin = io::stdin();
+    let mut stdin_iterator = stdin.lock().lines();
+
+    let mut fptr = File::create(env::var("OUTPUT_PATH").unwrap()).unwrap();
+
+    let _arr_count = stdin_iterator.next().unwrap().unwrap().trim().parse::<i32>().unwrap();
+
+    let arr: Vec<i32> = stdin_iterator.next().unwrap().unwrap()
+        .trim_end()
+        .split(' ')
+        .map(|s| s.to_string().parse::<i32>().unwrap())
+        .collect();
+
+    let result = migratoryBirds(&arr);
+
+    writeln!(&mut fptr, "{}", result).ok();
+}
